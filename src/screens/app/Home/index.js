38598826f63293ef6465d20aefed66db081988ca
Lucian Brooks <74628091+LucianAiolos@users.noreply.github.com>
2023-10-13 01:36:30 -0700

    Home statuscard components

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,19 +1,20 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import firestore from '@react-native-firebase/firestore';
-import { Text } from 'react-native'
+import { Text, ScrollView, View, SafeAreaView } from 'react-native'
 import styles from './styles'
-import { ScrollView } from 'react-native-gesture-handler'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../../../components/Header'
 import PlusIcon from '../../../components/PlusIcon'
 import Title from '../../../components/Title'
 import { useSelector, useDispatch } from 'react-redux'
 import { setTasks, setToUpdate } from '../../../redux/tasksSlice';
+import StatusCard from '../../../components/StatusCard';
+import moment from 'moment';
 
 const Home = ({navigation}) => {
   const user = useSelector(state => state.user.data)
   const tasks = useSelector(state => state.tasks.data)
   const toUpdate = useSelector(state => state.tasks.toUpdate)
+  const [counts, setCounts] = useState({})
   const dispatch = useDispatch()
   console.log('toUpdate HOME', toUpdate)
 
@@ -34,6 +35,32 @@ const Home = ({navigation}) => {
         dispatch(setTasks(tasksList))
       });
   }, [user, dispatch, toUpdate]);
+
+  useEffect (()=>  {
+    if(tasks?.length) {
+      console.log('tasks :>> IN HOME', tasks)
+      const highPriority = tasks?.filter(
+        task => task?.category === 'urgent' 
+        || 
+        task?.category === 'important'
+      )
+      const today = moment(new Date()).format('YYYY-MM-DD')
+      const dueDeadline = tasks?.filter(task => {
+        const deadline = task?.deadline?.seconds * 1000 // converting from milliseconds
+        const deadlineFormatted = moment(deadline).format('YYYY-MM-DD')
+        return moment(deadlineFormatted).isBefore(today)
+      })
+      const quickWin = tasks?.filter(
+        task => task?.category === 'quick_task' 
+      )
+
+      setCounts({
+        highPriority: highPriority?.length,
+        dueDeadline: dueDeadline?.length,
+        quickWin: quickWin?.length,
+      })
+    }
+  },[tasks])
   
   return (
     <SafeAreaView style={styles.container}>
@@ -42,6 +69,11 @@ const Home = ({navigation}) => {
       <ScrollView >
         <Title type='thin'>Daily Tasks</Title>
         
+        <View style={styles.row}>
+          <StatusCard label="High Priority" count={counts?.highPriority} type='error' />
+          <StatusCard label="Due Deadline" count={counts?.dueDeadline} />
+          <StatusCard label="Quick Win" count={counts?.quickWin} />
+        </View>
       </ScrollView>
 
       <PlusIcon />
--- src/screens/app/Home/styles.js
+++ src/screens/app/Home/styles.js
@@ -5,6 +5,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 24,
+  },
 })
 
 export default styles
